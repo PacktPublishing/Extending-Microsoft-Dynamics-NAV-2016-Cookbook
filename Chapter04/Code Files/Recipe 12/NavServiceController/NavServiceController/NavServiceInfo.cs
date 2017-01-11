8bdd5562f2d16aec7fd470c0af79b478b6471436
Dhiraj Chandanshive <dhirajc@packtpub.com>
2017-01-11 12:04:21 +0530

    Add files via upload

---
+++ Chapter04/Code Files/Recipe 12/NavServiceController/NavServiceController/NavServiceInfo.cs
@@ -0,0 +1,39 @@
+﻿using System.Xml;
+using System.ServiceProcess;
+
+namespace NavServiceController
+{
+    public static class NavServiceInfo
+    {
+        public static XmlDocument GetNavServices()
+        {
+            XmlDocument xmlDoc = new XmlDocument();
+            XmlElement rootNode = xmlDoc.CreateElement("Controllers");
+            xmlDoc.AppendChild(rootNode);
+
+            ServiceController[] controllers = ServiceController.GetServices();
+            foreach (ServiceController controller in controllers)
+                if (controller.ServiceName.StartsWith("MicrosoftDynamicsNavServer"))
+                    rootNode.AppendChild(CreateControllerNode(xmlDoc, controller));
+
+            return xmlDoc;
+        }
+
+        private static XmlElement CreateControllerNode(XmlDocument xmlDoc, ServiceController controller)
+        {
+            XmlElement controllerNode = xmlDoc.CreateElement("Controller");
+            AppendXmlChildNode(xmlDoc, controllerNode, "DisplayName", controller.DisplayName);
+            AppendXmlChildNode(xmlDoc, controllerNode, "ServiceName", controller.ServiceName);
+            AppendXmlChildNode(xmlDoc, controllerNode, "Status", controller.Status.ToString());
+
+            return controllerNode;
+        }
+
+        private static void AppendXmlChildNode(XmlDocument xmlDoc, XmlNode parentNode, string nodeName, string nodeValue)
+        {
+            XmlElement node = xmlDoc.CreateElement(nodeName);
+            node.InnerText = nodeValue;
+            parentNode.AppendChild(node);
+        }
+    }
+}
---
+++ Chapter04/Code Files/Recipe 12/NavServiceController/NavServiceController/NavServiceStatusController.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.ServiceProcess;
+using System.Threading.Tasks;
+
+namespace NavServiceController
+{
+    public class NavServiceStatusController
+    {
+        public event EventHandler<string> ServiceStarted;
+        public event EventHandler<string> ServiceStopped;
+        public event EventHandler<string> OperationFailed;
+
+        protected virtual void OnServiceStartedEvent(string serviceName)
+        {
+            EventHandler<string> handler = ServiceStarted;
+            handler(this, serviceName);
+        }
+
+        protected virtual void OnServiceStoppedEvent(string serviceName)
+        {
+            EventHandler<string> handler = ServiceStopped;
+            handler(this, serviceName);
+        }
+
+        protected virtual void OnOperationFailedEvent(string serviceName)
+        {
+            EventHandler<string> handler = OperationFailed;
+            handler(this, serviceName);
+        }
+
+        public bool StartService(string serviceName)
+        {
+            ServiceController controller = new ServiceController(serviceName);
+            if (controller.Status != ServiceControllerStatus.Stopped)
+                return false;
+
+            Task awaitResponse = Task.Run(() =>
+            {
+                    controller.Start();
+                    controller.WaitForStatus(ServiceControllerStatus.Running, new TimeSpan(30 * TimeSpan.TicksPerSecond));
+            });
+
+            awaitResponse.ContinueWith(antecedent => OnServiceStartedEvent(serviceName), TaskContinuationOptions.OnlyOnRanToCompletion | TaskContinuationOptions.ExecuteSynchronously);
+            awaitResponse.ContinueWith(antecedent => OnOperationFailedEvent(serviceName), TaskContinuationOptions.NotOnRanToCompletion | TaskContinuationOptions.ExecuteSynchronously);
+
+            return true;
+        }
+
+        [PermissionSet(SecurityAction.Demand, Name = "FullTrust")]
+        public bool StopService(string serviceName)
+        {
+            ServiceController controller = new ServiceController(serviceName);
+            if (controller.Status != ServiceControllerStatus.Running)
+                return false;
+
+            Task awaitResponse = Task.Run(() =>
+            {
+                    controller.Stop();
+                    controller.WaitForStatus(ServiceControllerStatus.Stopped, new TimeSpan(30 * TimeSpan.TicksPerSecond));
+            });
+
+            awaitResponse.ContinueWith(antecedent => OnServiceStoppedEvent(serviceName), TaskContinuationOptions.OnlyOnRanToCompletion | TaskContinuationOptions.ExecuteSynchronously);
+            awaitResponse.ContinueWith(antecedent => OnOperationFailedEvent(serviceName), TaskContinuationOptions.NotOnRanToCompletion | TaskContinuationOptions.ExecuteSynchronously);
+
+            return true;
+        }
+    }
+}
